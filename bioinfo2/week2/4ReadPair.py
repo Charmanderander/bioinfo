4ff43af8c47ad28003de586a99c2fba15dec8693
jinhao <jinhao_@hotmail.com>
2018-07-07 22:00:52 +0800

    stop at week 2 problem 4

--- bioinfo2/week2/4ReadPair.py
+++ bioinfo2/week2/4ReadPair.py
@@ -1,13 +1,32 @@
-import sys
-
 def EulerPath(adjDict):
     path = []
     stack = []
-    reversedPath = GeneratePath(adjDict,path, stack, 2)
+    reversedPath = GeneratePath(adjDict,path, stack)
     path = reversedPath[::-1]
-    
+
     return path
-    
+
+def GeneratePairDict(dataset):
+    ''' Each dataset has a pair of strings
+    We generate a dictionary whose key is a list of prefixs
+    the prefix key will point to the suffix values
+    '''
+
+    pairDict = {}
+
+    for item in dataset:
+        pairs = item.split('|')
+        prefixString = ""
+        suffixString = ""
+
+        for string in pairs:
+            prefixString += Prefix(string)
+            suffixString += Suffix(string)
+        
+        pairDict[prefixString] = [suffixString]
+
+    return pairDict
+
 def FindUnbalancedNode(adjDict):
     # We modify the list and close the loop
     outCount = {}
@@ -16,13 +35,13 @@ def FindUnbalancedNode(adjDict):
     keys = adjDict.keys()
 
     possibleNodes = []
-    
+
     for key in keys:
         possibleNodes += [item for item in adjDict[key]]
 
-    possibleNodes = possibleNodes + list(set(keys) - set(possibleNodes)) 
+    possibleNodes = possibleNodes + list(set(keys) - set(possibleNodes))
 
-    unbalancedIn = unbalancedOut = 0
+    unbalancedIn = unbalancedOut = ""
 
     for key1 in possibleNodes:
         if key1 in keys:
@@ -32,31 +51,40 @@ def FindUnbalancedNode(adjDict):
         inCount[key1] = 0
         for key2 in keys:
             inCount[key1] += adjDict[key2].count(key1)
-        
+
         if outCount[key1] > inCount[key1]:
             unbalancedOut = key1
         if outCount[key1] < inCount[key1]:
             unbalancedIn = key1
 
+    print(inCount)
+    print(outCount)
+
     # closing the loop
-    if unbalancedIn != 0 and unbalancedOut != 0:
-        adjDict[unbalancedIn] = [unbalancedOut]
+    if unbalancedIn != "" and unbalancedOut != "":
+        if unbalancedIn!=unbalancedOut:
+            adjDict[unbalancedIn].append(unbalancedOut)
+    else:
+        unbalancedIn = 0
+        unbalancedOut = 0
 
     return adjDict, unbalancedOut
 
-def GeneratePath(adjDict, path, stack, dst):
+
+def GeneratePath(adjDict, path, stack):
+    dst = list(adjDict.keys())[0]
     while True:
         if len(list(adjDict[dst])) == 0:
             # If current vertex has no out-going edges (i.e. neighbors)
             # add it to circuit
-            path.append(dst)      
+            path.append(dst)
             if stack == []:
                 break
             # set last vertex from the stack as the current one
             dst = stack[-1]
             # remove the last vertex from the stack
             stack.pop()
-   
+
         else:
             # Otherwise (in case it has out-going edges, i.e. neighbors)
             # add the vertex to the stack
@@ -66,31 +94,54 @@ def GeneratePath(adjDict, path, stack, dst):
             # remove the edge between that vertex and selected neighbor
             adjDict[dst].pop(0)
             dst = newdst
-       
 
     return path
+
+def Prefix(string):
+    return string[:-1]
+
+def Suffix(string):
+    return string [1:]
 
 def rotate(l, n):
     return l[n:] + l[:n]
 
-adjDict = {}
-'''
-data = sys.stdin.read().split('\n')
-'''
-
 with open("data.txt", "r") as f:
     data = f.readlines()
 
+k = 3 
+d = 1
+
+dataList = []
+
 for item in data:
-        adjDict[int(item.split("->")[0])] = list(map(int,item.split("->")[1].strip().split(",")))
+    cleanString = item.strip().replace('\n', '')
+    dataList.append(cleanString)
 
-adjDict, unbalancedOut = FindUnbalancedNode(adjDict)
+adjDict = GeneratePairDict(dataList)
+print(adjDict)
+newAdjDict, unbalancedOut = FindUnbalancedNode(adjDict)
+print("###")
+print(newAdjDict)
+ans = EulerPath(newAdjDict)
 
-ans = EulerPath(adjDict)
 ans.pop(0)
 #rotate the list to bring the unbalanced node to the head
 rotated = rotate(ans, ans.index(unbalancedOut))
 
-print "->".join(map(str,rotated))
+print(rotated)
 
+ansStr = rotated[0][:k-1]
 
+rotated.pop(0)
+
+for item in rotated:
+    print(item)
+    ansStr += item[k-2]
+
+ansStr += rotated[-k][k-1:]
+
+for item in rotated[-(k-1):]:
+    ansStr += item[-1]
+
+print(ansStr)
